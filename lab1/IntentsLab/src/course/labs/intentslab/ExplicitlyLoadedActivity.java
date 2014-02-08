86d04e266dcbd5911d6cbd69f956d95e4c477e1c
Larry Fenske <github@towanda.com>
2014-02-08 13:33:50 -0700

    Lab 1 passes submission.

--- lab1/IntentsLab/src/course/labs/intentslab/ExplicitlyLoadedActivity.java
+++ lab1/IntentsLab/src/course/labs/intentslab/ExplicitlyLoadedActivity.java
@@ -46,13 +46,14 @@ public class ExplicitlyLoadedActivity extends Activity {
 
 		Log.i(TAG,"Entered enterClicked()");
 		
-		// TODO - Save user provided input from the EditText field
-
-		// TODO - Create a new intent and save the input from the EditText field as an extra
-		
-		// TODO - Set Activity's result with result code RESULT_OK
-		
-		// TODO - Finish the Activity
-
+		// Save user provided input from the EditText field
+		CharSequence text = mEditText.getText();
+		// Create a new intent and save the input from the EditText field as an extra
+		Intent resultIntent = new Intent();
+		resultIntent.putExtra("textfield", text);
+		// Set Activity's result with result code RESULT_OK
+		setResult(RESULT_OK, resultIntent);
+		// Finish the Activity
+		finish();
 	}
 }
